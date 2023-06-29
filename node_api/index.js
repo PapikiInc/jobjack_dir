6fb3e6758010c31bcd911a5574842d7ccf63c7d5
Sone <Sone@incendiaryblue.com>
2023-06-29 04:54:34 +0200

    api works able to get dir listing

--- node_api/index.js
+++ node_api/index.js
@@ -20,8 +20,34 @@ app.get('/directory', (req, res) => {
             console.error(err);
             return res.status(500).json({ error: 'Failed to read directory'});
         }
-    })
+
+        const directoryListing = files.map((file) => {
+            const fullPath = path.join(directoryPath, file.name);
+            const stats = fs.statSync(fullPath);
+
+            return{
+                name: file.name,
+                fullPath: fullPath,
+                size: stats.size,
+                extension: path.extname(file.name),
+                createdDate: stats.birthtime,
+                isDirectory: file.isDirectory(),
+                permissions: getPermissions(stats)
+            };
+        });
+
+        res.json({ directoryListing });
+    });
 });
+
+// Function for permissions on directory
+function getPermissions(stats) {
+    return {
+      read: !!(stats.mode & 0o400),
+      write: !!(stats.mode & 0o200),
+      execute: !!(stats.mode & 0o100)
+    };
+  }
 
 //node run
 const port = process.env.PORT || 3000;
